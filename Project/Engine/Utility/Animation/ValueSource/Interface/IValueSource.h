dade774cc10e242967c49ea3802802b861857239
MurataSakuya <144077681+Muratasakuya@users.noreply.github.com>
2026-02-01 03:40:45 +0900

    パネルバグ修正

--- Project/Engine/Utility/Animation/ValueSource/Interface/IValueSource.h
+++ Project/Engine/Utility/Animation/ValueSource/Interface/IValueSource.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 
 //============================================================================
 //	include
@@ -11,37 +11,36 @@
 //============================================================================
 namespace SakuEngine {
 
-template <typename T>
-class IValueSource {
-public:
-	//========================================================================
-	//	public Methods
-	//========================================================================
+	template <typename T>
+	class IValueSource {
+	public:
+		//========================================================================
+		//	public Methods
+		//========================================================================
 
-	IValueSource() = default;
-	virtual ~IValueSource() = default;
+		IValueSource() = default;
+		virtual ~IValueSource() = default;
 
-	// 更新開始
-	virtual void Start(const T& base) = 0;
+		// 更新開始
+		virtual void Start(const T& base) = 0;
 
-	// 値の更新
-	virtual void Update() = 0;
+		// 値の更新
+		virtual void Update() = 0;
 
-	// 値の取得
-	virtual T GetValue() const = 0;
+		// 値の取得
+		virtual T GetValue() const = 0;
 
-	// 終了判定
-	virtual bool IsFinished() const = 0;
+		// 終了判定
+		virtual bool IsFinished() const = 0;
 
-	// リセット
-	virtual void Reset() = 0;
+		// リセット
+		virtual void Reset() = 0;
 
-	// エディター
-	virtual void ImGui(const char* label) = 0;
+		// エディター
+		virtual void ImGui(const char* label) = 0;
 
-	// json
-	virtual void FromJson(const Json& data) = 0;
-	virtual void ToJson(Json& data) = 0;
-};
-
+		// json
+		virtual void FromJson(const Json& data) = 0;
+		virtual void ToJson(Json& data) = 0;
+	};
 }; // SakuEngine
